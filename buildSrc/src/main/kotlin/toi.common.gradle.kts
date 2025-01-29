e57f3404decf2011ff8a88b7504b120329b8c30a
frodank <11638682+frodank@users.noreply.github.com>
2025-01-29 16:58:44 +0100

    Trenger common dependencies

--- buildSrc/src/main/kotlin/toi.common.gradle.kts
+++ buildSrc/src/main/kotlin/toi.common.gradle.kts
@@ -12,6 +12,15 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    implementation("org.slf4j:slf4j-api:2.0.3")
+    implementation("ch.qos.logback:logback-classic:1.4.4")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.2")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.1")
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
 
 val stiTilApplicationClass =
