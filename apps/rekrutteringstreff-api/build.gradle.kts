247f0a83d4cdd03f501f22821fb851d47c459c36
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-02-04 13:42:19 +0100

    Legg til swagger på isready og isalive

--- apps/rekrutteringstreff-api/build.gradle.kts
+++ apps/rekrutteringstreff-api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("toi.common")
+    kotlin("kapt")
 }
 
 val javalinVersion = "6.4.0"
@@ -9,6 +10,7 @@ val flywayVersion = "11.3.0"
 val postgresVersion = "42.7.5"
 val hikariVersion = "6.2.1"
 val testContainerVersion = "1.20.4"
+val swaggerParserVersion = "2.1.12"
 
 dependencies {
     implementation("org.flywaydb:flyway-core:$flywayVersion")
@@ -17,7 +19,15 @@ dependencies {
     implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
     implementation("com.github.kittinunf.fuel:fuel-jackson:$fuelVersion")
     implementation("io.javalin:javalin:$javalinVersion")
+
+    kapt("io.javalin.community.openapi:openapi-annotation-processor:$javalinVersion")
+    implementation("io.javalin.community.openapi:javalin-openapi-plugin:$javalinVersion")
+    implementation("io.javalin.community.openapi:javalin-swagger-plugin:$javalinVersion")
+    implementation("io.javalin.community.openapi:openapi-specification:$javalinVersion")
+    kapt("io.swagger.parser.v3:swagger-parser:$swaggerParserVersion")
+
     testImplementation("org.testcontainers:testcontainers:$testContainerVersion")
     testImplementation("org.testcontainers:postgresql:$testContainerVersion")
     testImplementation("no.nav.security:mock-oauth2-server:$mockOAuth2ServerVersion")
+
 }
--- apps/rekrutteringstreff-api/src/main/kotlin/no/nav/toi/rekrutteringstreff/KubernetesHealth.kt
+++ apps/rekrutteringstreff-api/src/main/kotlin/no/nav/toi/rekrutteringstreff/KubernetesHealth.kt
@@ -1,15 +1,42 @@
 package no.nav.toi.rekrutteringstreff
 
 import io.javalin.Javalin
+import io.javalin.openapi.HttpMethod
+import io.javalin.openapi.OpenApi
+import io.javalin.openapi.OpenApiContent
+import io.javalin.openapi.OpenApiResponse
 
 private const val endepunktReady = "/isready"
 private const val endepunktAlive = "/isalive"
 
+@OpenApi(
+    summary = "isready",
+    operationId = "todo",
+    tags = [],
+    //requestBody = OpenApiRequestBody([OpenApiContent(String::class)]),
+    responses = [OpenApiResponse("200", [OpenApiContent(String::class)])],
+    path = "/isready",
+    methods = [HttpMethod.POST]
+)
+fun isReadyHandler(ctx: io.javalin.http.Context) {
+    ctx.result("isready")
+}
+
+@OpenApi(
+    summary = "isalive",
+    operationId = "todo",
+    tags = [],
+    //requestBody = OpenApiRequestBody([OpenApiContent(String::class)]),
+    responses = [OpenApiResponse("200", [OpenApiContent(String::class)])],
+    path = "/isalive",
+    methods = [HttpMethod.POST]
+)
+fun isAliveHandler(ctx: io.javalin.http.Context) {
+    ctx.result("isalive")
+}
+
+
 fun Javalin.handleHealth() {
-    get(endepunktReady) { ctx->
-        ctx.result("isReady")
-    }
-    get(endepunktAlive) { ctx->
-        ctx.result("isAlive")
-    }
+    get(endepunktReady, ::isReadyHandler)
+    get(endepunktAlive, ::isAliveHandler)
 }
