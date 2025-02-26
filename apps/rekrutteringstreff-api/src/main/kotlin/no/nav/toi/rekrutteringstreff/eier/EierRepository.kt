29ddf25050d69e4c39ff00c93379e0260a2b99cb
frodank <11638682+frodank@users.noreply.github.com>
2025-02-26 16:24:41 +0100

    Refaktorere bort idKolonne i eier-repo

--- apps/rekrutteringstreff-api/src/main/kotlin/no/nav/toi/rekrutteringstreff/eier/EierRepository.kt
+++ apps/rekrutteringstreff-api/src/main/kotlin/no/nav/toi/rekrutteringstreff/eier/EierRepository.kt
@@ -5,43 +5,54 @@ import no.nav.toi.rekrutteringstreff.Tabellnavn
 import no.nav.toi.rekrutteringstreff.TreffId
 import javax.sql.DataSource
 
-
 class EierRepository(
     private val dataSource: DataSource,
     private val rekrutteringstreff: Tabellnavn,
     private val eiere: Kolonnenavn,
-    id: Kolonnenavn
+    private val id: Kolonnenavn
 ) {
-    private val idKolonne = id
-    fun hent(id: TreffId): List<Eier>? {
+    fun hent(treff: TreffId): List<Eier>? {
         dataSource.connection.use { connection ->
-            connection.prepareStatement("SELECT $eiere FROM $rekrutteringstreff WHERE $idKolonne = ?").use { stmt ->
-                stmt.setObject(1, id.somUuid)
-                val resultSet = stmt.executeQuery()
-                return if (resultSet.next()) {
-                    (resultSet.getArray("$eiere").array as Array<*>)
-                        .map(Any?::toString)
-                        .map(::Eier)
-                } else null
-            }
+            connection.prepareStatement("SELECT $eiere FROM $rekrutteringstreff WHERE $id = ?")
+                .use { stmt ->
+                    stmt.setObject(1, treff.somUuid)
+                    val resultSet = stmt.executeQuery()
+                    return if (resultSet.next()) {
+                        (resultSet.getArray("$eiere").array as Array<*>)
+                            .map(Any?::toString)
+                            .map(::Eier)
+                    } else null
+                }
         }
     }
 
-    fun leggTilEiere(id: TreffId, nyeEiere: List<String>) {
+    fun leggTilEiere(treff: TreffId, nyeEiere: List<String>) {
         dataSource.connection.use { connection ->
-            connection.prepareStatement("UPDATE $rekrutteringstreff SET $eiere = array(SELECT DISTINCT unnest(array_cat($eiere, ?))) WHERE $idKolonne = ?").use { stmt ->
+            connection.prepareStatement(
+                """
+                    UPDATE $rekrutteringstreff
+                    SET $eiere = array(SELECT DISTINCT unnest(array_cat($eiere, ?)))
+                    WHERE $id = ?
+                """.trimIndent()
+            ).use { stmt ->
                 stmt.setArray(1, connection.createArrayOf("text", nyeEiere.toTypedArray()))
-                stmt.setObject(2, id.somUuid)
+                stmt.setObject(2, treff.somUuid)
                 stmt.executeUpdate()
             }
         }
     }
 
-    fun slett(id: TreffId, eier: String) {
+    fun slett(treff: TreffId, eier: String) {
         dataSource.connection.use { connection ->
-            connection.prepareStatement("UPDATE $rekrutteringstreff SET $eiere = array_remove($eiere, ?) WHERE $idKolonne = ?").use { stmt ->
+            connection.prepareStatement(
+                """
+                    UPDATE $rekrutteringstreff
+                    SET $eiere = array_remove($eiere, ?) 
+                    WHERE $id = ?
+                """.trimIndent()
+            ).use { stmt ->
                 stmt.setString(1, eier)
-                stmt.setObject(2, id.somUuid)
+                stmt.setObject(2, treff.somUuid)
                 stmt.executeUpdate()
             }
         }
