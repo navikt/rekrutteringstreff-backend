660bb00e974a6e399b1341f82824837c4f3025ef
Are Husby <are.husby@nav.no>
2025-03-11 12:49:27 +0100

    Første røde test

---
+++ apps/rekrutteringstreff-api/src/test/kotlin/no/nav/toi/rekrutteringstreff/arbeidsgiver/ArbeidsgiverTest.kt
@@ -0,0 +1,98 @@
+package no.nav.toi.rekrutteringstreff.arbeidsgiver
+
+import com.github.kittinunf.fuel.Fuel
+import no.nav.security.mock.oauth2.MockOAuth2Server
+import no.nav.toi.*
+import no.nav.toi.rekrutteringstreff.TestDatabase
+import no.nav.toi.ubruktPortnrFra10000.ubruktPortnr
+import org.junit.jupiter.api.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.net.HttpURLConnection.HTTP_CREATED
+import java.net.HttpURLConnection.HTTP_UNAUTHORIZED
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ArbeidsgiverTest {
+
+
+    companion object {
+        private val authServer = MockOAuth2Server()
+        private val authPort = 18012
+        private val database = TestDatabase()
+        private val appPort = ubruktPortnr()
+
+        private val app = App(
+            port = appPort,
+            authConfigs = listOf(
+                AuthenticationConfiguration(
+                    issuer = "http://localhost:$authPort/default",
+                    jwksUri = "http://localhost:$authPort/default/jwks",
+                    audience = "rekrutteringstreff-audience"
+                )
+            ),
+            database.dataSource
+        )
+    }
+
+    @BeforeAll
+    fun setUp() {
+        authServer.start(port = authPort)
+        app.start()
+    }
+
+    @AfterAll
+    fun tearDown() {
+        authServer.shutdown()
+        app.close()
+    }
+
+    @AfterEach
+    fun reset() {
+        database.slettAlt()
+    }
+
+    data class Orgnr(val orgnr: String) {
+        init {
+            if (!(orgnr.length == 9 && orgnr.all(Char::isDigit))) throw IllegalArgumentException("Orgnr må være 9 siffer. Mottok [$orgnr].")
+        }
+
+        val asString = orgnr
+        override fun toString(): String = asString
+
+    }
+
+
+    data class Arbeidsgiver(val orgnr: Orgnr, val orgnavn: String, val status: String = "dummyStatus") // TODO: Flytt til prodkoden
+
+    fun tokenVarianter() = UautentifiserendeTestCase.somStrømAvArgumenter()
+
+    @ParameterizedTest
+    @MethodSource("tokenVarianter")
+    fun autentiseringLeggTilArbeidsgiver(autentiseringstest: UautentifiserendeTestCase) {
+        val leggPåToken = autentiseringstest.leggPåToken
+        val anyTreffId = "anyTreffID"
+        val (_, response, result) = Fuel.post("http://localhost:${appPort}/api/rekrutteringstreff/$anyTreffId/arbeidsgiver")
+            .leggPåToken(authServer, authPort)
+            .responseString()
+        assertStatuscodeEquals(HTTP_UNAUTHORIZED, response, result)
+    }
+
+
+    @Test
+    fun leggTilArbeidsgiver() {
+        val navIdent = "A123456"
+        val token = authServer.lagToken(authPort, navIdent = navIdent)
+        val anyOrgnr = Orgnr("123456789")
+        val anyTreffId = "anyTreffID"
+        val arbeidsgiver = Arbeidsgiver(anyOrgnr, "anyOrgnavn")
+
+        val (_, response, result) = Fuel.post("http://localhost:$appPort/api/rekrutteringstreff/$anyTreffId/arbeidsgiver")
+            .body(mapper.writeValueAsString(arbeidsgiver))
+            .header("Authorization", "Bearer ${token.serialize()}")
+            .responseString()
+
+        assertStatuscodeEquals(HTTP_CREATED, response, result)
+    }
+
+}
