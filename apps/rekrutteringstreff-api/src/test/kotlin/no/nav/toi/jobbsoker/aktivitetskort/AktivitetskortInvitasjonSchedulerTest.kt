3524cc8684d4fcbd98bfca3ded62540e6ada93d9
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-08-04 12:18:51 +0200

    Legg til flwyay for aktivitetskort invitasjon test

--- apps/rekrutteringstreff-api/src/test/kotlin/no/nav/toi/jobbsoker/aktivitetskort/AktivitetskortInvitasjonSchedulerTest.kt
+++ apps/rekrutteringstreff-api/src/test/kotlin/no/nav/toi/jobbsoker/aktivitetskort/AktivitetskortInvitasjonSchedulerTest.kt
@@ -1,18 +1,12 @@
 package no.nav.toi.jobbsoker.aktivitetskort
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.toi.*
-import no.nav.toi.jobbsoker.Etternavn
-import no.nav.toi.jobbsoker.Fornavn
-import no.nav.toi.jobbsoker.Fødselsnummer
-import no.nav.toi.jobbsoker.JobbsøkerRepository
-import no.nav.toi.jobbsoker.Kandidatnummer
-import no.nav.toi.jobbsoker.LeggTilJobbsøker
-import no.nav.toi.jobbsoker.Navkontor
-import no.nav.toi.jobbsoker.VeilederNavIdent
-import no.nav.toi.jobbsoker.VeilederNavn
+import no.nav.toi.jobbsoker.*
+import no.nav.toi.rekrutteringstreff.RekrutteringstreffRepository
 import no.nav.toi.rekrutteringstreff.TestDatabase
 import org.assertj.core.api.Assertions.assertThat
+import org.flywaydb.core.Flyway
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -21,33 +15,53 @@ import java.util.*
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AktivitetskortInvitasjonSchedulerTest {
 
-    private val testDatabase = TestDatabase()
-    private val jobbsøkerRepository = JobbsøkerRepository(testDatabase.dataSource, jacksonObjectMapper())
+    companion object {
+        private val db = TestDatabase()
+        private lateinit var jobbsøkerRepository: JobbsøkerRepository
+        private lateinit var aktivitetskortInvitasjonRepository: AktivitetskortInvitasjonRepository
+        private lateinit var rekrutteringstreffRepository: RekrutteringstreffRepository
+        private val mapper = JacksonConfig.mapper
 
-    private val aktivitetskortInvitasjonRepository = AktivitetskortInvitasjonRepository(testDatabase.dataSource)
-    private val rekrutteringstreffRepository = no.nav.toi.rekrutteringstreff.RekrutteringstreffRepository(testDatabase.dataSource)
+
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            Flyway.configure().dataSource(db.dataSource).load().migrate()
+            jobbsøkerRepository = JobbsøkerRepository(db.dataSource, mapper)
+            aktivitetskortInvitasjonRepository = AktivitetskortInvitasjonRepository(db.dataSource)
+            rekrutteringstreffRepository =
+                RekrutteringstreffRepository(db.dataSource)
+
+        }
+    }
+
 
     @BeforeEach
     fun setup() {
-        testDatabase.slettAlt()
+        db.slettAlt()
     }
 
     @Test
     fun `skal sende invitasjoner på rapid og markere dem som pollet`() {
         // Arrange
         val rapid = TestRapid()
-        val scheduler = AktivitetskortInvitasjonScheduler(aktivitetskortInvitasjonRepository, rekrutteringstreffRepository, rapid)
-        val treffId = testDatabase.opprettRekrutteringstreffIDatabase()
+        val scheduler =
+            AktivitetskortInvitasjonScheduler(aktivitetskortInvitasjonRepository, rekrutteringstreffRepository, rapid)
+        val treffId = db.opprettRekrutteringstreffIDatabase()
         val fødselsnummer = Fødselsnummer("12345678901")
-        jobbsøkerRepository.leggTil(listOf(LeggTilJobbsøker(
-            fødselsnummer = fødselsnummer,
-            kandidatnummer = Kandidatnummer("ABC123"),
-            fornavn = Fornavn("Ola"),
-            etternavn = Etternavn("Nordmann"),
-            navkontor = Navkontor("Oslo"),
-            veilederNavn = VeilederNavn("Kari Veileder"),
-            veilederNavIdent = VeilederNavIdent("Z123456"),
-        )), treffId, "Z123456")
+        jobbsøkerRepository.leggTil(
+            listOf(
+                LeggTilJobbsøker(
+                    fødselsnummer = fødselsnummer,
+                    kandidatnummer = Kandidatnummer("ABC123"),
+                    fornavn = Fornavn("Ola"),
+                    etternavn = Etternavn("Nordmann"),
+                    navkontor = Navkontor("Oslo"),
+                    veilederNavn = VeilederNavn("Kari Veileder"),
+                    veilederNavIdent = VeilederNavIdent("Z123456"),
+                )
+            ), treffId, "Z123456"
+        )
         val personTreffId = jobbsøkerRepository.hentJobbsøker(treffId, fødselsnummer)!!.personTreffId
         jobbsøkerRepository.inviter(listOf(personTreffId), treffId, "Z123456")
 
@@ -68,18 +82,23 @@ class AktivitetskortInvitasjonSchedulerTest {
     fun `skal ikke sende samme invitasjon to ganger`() {
         // Arrange
         val rapid = TestRapid()
-        val scheduler = AktivitetskortInvitasjonScheduler(aktivitetskortInvitasjonRepository, rekrutteringstreffRepository, rapid)
-        val treffId = testDatabase.opprettRekrutteringstreffIDatabase()
+        val scheduler =
+            AktivitetskortInvitasjonScheduler(aktivitetskortInvitasjonRepository, rekrutteringstreffRepository, rapid)
+        val treffId = db.opprettRekrutteringstreffIDatabase()
         val fødselsnummer = Fødselsnummer("12345678901")
-        jobbsøkerRepository.leggTil(listOf(LeggTilJobbsøker(
-            fødselsnummer = fødselsnummer,
-            kandidatnummer = Kandidatnummer("ABC123"),
-            fornavn = Fornavn("Ola"),
-            etternavn = Etternavn("Nordmann"),
-            navkontor = Navkontor("Oslo"),
-            veilederNavn = VeilederNavn("Kari Veileder"),
-            veilederNavIdent = VeilederNavIdent("Z123456"),
-        )), treffId, "Z123456")
+        jobbsøkerRepository.leggTil(
+            listOf(
+                LeggTilJobbsøker(
+                    fødselsnummer = fødselsnummer,
+                    kandidatnummer = Kandidatnummer("ABC123"),
+                    fornavn = Fornavn("Ola"),
+                    etternavn = Etternavn("Nordmann"),
+                    navkontor = Navkontor("Oslo"),
+                    veilederNavn = VeilederNavn("Kari Veileder"),
+                    veilederNavIdent = VeilederNavIdent("Z123456"),
+                )
+            ), treffId, "Z123456"
+        )
         val personTreffId = jobbsøkerRepository.hentJobbsøker(treffId, fødselsnummer)!!.personTreffId
         jobbsøkerRepository.inviter(listOf(personTreffId), treffId, "Z123456")
 
@@ -95,7 +114,8 @@ class AktivitetskortInvitasjonSchedulerTest {
     fun `skal ikke gjøre noe hvis det ikke er noen usendte invitasjoner`() {
         // Arrange
         val rapid = TestRapid()
-        val scheduler = AktivitetskortInvitasjonScheduler(aktivitetskortInvitasjonRepository, rekrutteringstreffRepository, rapid)
+        val scheduler =
+            AktivitetskortInvitasjonScheduler(aktivitetskortInvitasjonRepository, rekrutteringstreffRepository, rapid)
 
         // Act
         scheduler.behandleInvitasjoner()
