43d90cb478c116c1a86be613356aed848e481957
Vilde Stefferud <vilde.stefferud@nav.no>
2025-08-26 12:23:28 +0200

    La til cascade delete
    Co-authored-by: Joar Aurdal <13847640+joarau@users.noreply.github.com>

--- apps/rekrutteringstreff-api/src/main/resources/db/migration/V15__næringskode_og_arbeidsgiver.sql
+++ apps/rekrutteringstreff-api/src/main/resources/db/migration/V15__næringskode_og_arbeidsgiver.sql
@@ -4,5 +4,11 @@ CREATE TABLE naringskode
     arbeidsgiver_db_id BIGINT NOT NULL,
     kode        text UNIQUE,
     beskrivelse text,
-    FOREIGN KEY (arbeidsgiver_db_id) REFERENCES arbeidsgiver (db_id)
+    CONSTRAINT naringskode_arbeidsgiver_fk
+        FOREIGN KEY (arbeidsgiver_db_id)
+            REFERENCES arbeidsgiver (db_id)
+            ON DELETE CASCADE,
+    CONSTRAINT naringskode_unik_per_arbeidsgiver UNIQUE (arbeidsgiver_db_id, kode)
 );
+
+CREATE INDEX naringskode_arbeidsgiver_db_id_idx ON naringskode (arbeidsgiver_db_id);
