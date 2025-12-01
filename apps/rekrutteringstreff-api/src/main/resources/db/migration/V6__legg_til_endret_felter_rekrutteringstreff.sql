605dda8da533c0f45cb515f73fd4704a4750c25c
Martine Erdal Mansaker <martine.erdal.mansaker@nav.no>
2025-12-01 12:08:55 +0100

    Endre til små bokstaver

--- apps/rekrutteringstreff-api/src/main/resources/db/migration/V6__legg_til_endret_felter_rekrutteringstreff.sql
+++ apps/rekrutteringstreff-api/src/main/resources/db/migration/V6__legg_til_endret_felter_rekrutteringstreff.sql
@@ -1,3 +1,3 @@
 ALTER TABLE rekrutteringstreff
-    ADD COLUMN sist_endret timestamp with time zone not null DEFAULT now(),
+    ADD COLUMN sist_endret timestamp with time zone not null default now(),
     ADD COLUMN sist_endret_av text;
