3aece34cb318d3c3c17a278ab4f8e859d1c0aa5e
haugse <sara.haugse@gmail.com>
2026-02-19 07:34:42 +0100

    Forsøk å flytte rapids og rivers-bibliotek inn i appen som bruker de

--- apps/rekrutteringsbistand-aktivitetskort/build.gradle.kts
+++ apps/rekrutteringsbistand-aktivitetskort/build.gradle.kts
@@ -9,6 +9,7 @@ val testContainerVersion = "2.0.3"
 val postgresTestContainerVersion = "1.21.4"
 val javalinVersion = "6.7.0"
 val micrometerVersion = "1.15.2"
+val tbdLibsVersion = "2026.01.28-07.21-5436e475"
 
 dependencies {
     implementation("io.javalin:javalin:${javalinVersion}")
@@ -22,6 +23,12 @@ dependencies {
     implementation("com.zaxxer:HikariCP:$hikariVersion")
     implementation("com.auth0:java-jwt:4.4.0")
     implementation("com.auth0:jwks-rsa:0.22.1")
+
+    // Rapids and rivers fra tbd-libs (uten Ktor)
+    implementation("com.github.navikt.tbd-libs:rapids-and-rivers:${tbdLibsVersion}")
+    implementation("com.github.navikt.tbd-libs:rapids-and-rivers-api:${tbdLibsVersion}")
+    implementation("com.github.navikt.tbd-libs:kafka:${tbdLibsVersion}")
+    testImplementation("com.github.navikt.tbd-libs:rapids-and-rivers-test:${tbdLibsVersion}")
 
     testImplementation("org.testcontainers:testcontainers:$testContainerVersion")
     testImplementation("org.testcontainers:postgresql:$postgresTestContainerVersion")
