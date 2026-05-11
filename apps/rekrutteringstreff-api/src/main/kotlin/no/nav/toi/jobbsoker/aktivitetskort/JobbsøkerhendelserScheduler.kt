4d211d7baf1a3c131f213b65143a9e6cfccc2159
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2026-05-11 11:43:47 +0200

    Flytt AktivitetskortHendelseskontekst inn i hendelsesfilen

--- apps/rekrutteringstreff-api/src/main/kotlin/no/nav/toi/jobbsoker/aktivitetskort/JobbsøkerhendelserScheduler.kt
+++ apps/rekrutteringstreff-api/src/main/kotlin/no/nav/toi/jobbsoker/aktivitetskort/JobbsøkerhendelserScheduler.kt
@@ -200,6 +200,49 @@ class JobbsøkerhendelserScheduler(
             .publiserTilRapids(rapidsConnection)
     }
 
+    private fun JobbsøkerHendelsestype.tilAktivitetskortHendelseskontekst(): AktivitetskortHendelseskontekst? =
+        when (this) {
+            JobbsøkerHendelsestype.INVITERT ->
+                AktivitetskortHendelseskontekst.Invitasjon
+            JobbsøkerHendelsestype.SVART_JA_TIL_INVITASJON ->
+                AktivitetskortHendelseskontekst.SvarOgTreffstatus(svar = true, svarAvgittAvEier = false)
+            JobbsøkerHendelsestype.SVART_JA_TIL_INVITASJON_AV_EIER ->
+                AktivitetskortHendelseskontekst.SvarOgTreffstatus(svar = true, svarAvgittAvEier = true)
+            JobbsøkerHendelsestype.SVART_NEI_TIL_INVITASJON ->
+                AktivitetskortHendelseskontekst.SvarOgTreffstatus(svar = false, svarAvgittAvEier = false)
+            JobbsøkerHendelsestype.SVART_NEI_TIL_INVITASJON_AV_EIER ->
+                AktivitetskortHendelseskontekst.SvarOgTreffstatus(svar = false, svarAvgittAvEier = true)
+            JobbsøkerHendelsestype.SVAR_FJERNET_AV_EIER ->
+                AktivitetskortHendelseskontekst.SvarOgTreffstatus(svar = null, svarAvgittAvEier = true)
+            JobbsøkerHendelsestype.SVART_JA_TREFF_AVLYST ->
+                AktivitetskortHendelseskontekst.SvarOgTreffstatus(svar = true, svarAvgittAvEier = false, treffstatus = AktivitetskortTreffstatus.AVLYST)
+            JobbsøkerHendelsestype.SVART_JA_TREFF_FULLFØRT ->
+                AktivitetskortHendelseskontekst.SvarOgTreffstatus(svar = true, svarAvgittAvEier = false, treffstatus = AktivitetskortTreffstatus.FULLFØRT)
+            JobbsøkerHendelsestype.IKKE_SVART_TREFF_AVLYST ->
+                AktivitetskortHendelseskontekst.SvarOgTreffstatus(svar = null, svarAvgittAvEier = false, treffstatus = AktivitetskortTreffstatus.AVLYST)
+            JobbsøkerHendelsestype.IKKE_SVART_TREFF_FULLFØRT ->
+                AktivitetskortHendelseskontekst.SvarOgTreffstatus(svar = null, svarAvgittAvEier = false, treffstatus = AktivitetskortTreffstatus.FULLFØRT)
+            JobbsøkerHendelsestype.TREFF_ENDRET_ETTER_PUBLISERING ->
+                AktivitetskortHendelseskontekst.Treffoppdatering(inkluderEndringsnotifikasjon = false)
+            JobbsøkerHendelsestype.TREFF_ENDRET_ETTER_PUBLISERING_NOTIFIKASJON ->
+                AktivitetskortHendelseskontekst.Treffoppdatering(inkluderEndringsnotifikasjon = true)
+            else -> null
+        }
+
+    private sealed interface AktivitetskortHendelseskontekst {
+        data object Invitasjon : AktivitetskortHendelseskontekst
+
+        data class SvarOgTreffstatus(
+            val svar: Boolean?,
+            val svarAvgittAvEier: Boolean,
+            val treffstatus: AktivitetskortTreffstatus? = null,
+        ) : AktivitetskortHendelseskontekst
+
+        data class Treffoppdatering(
+            val inkluderEndringsnotifikasjon: Boolean,
+        ) : AktivitetskortHendelseskontekst
+    }
+
     private data class JobbsøkerHendelseForAktivitetskort(
         val jobbsokerHendelseDbId: Long,
         val hendelseId: UUID,
