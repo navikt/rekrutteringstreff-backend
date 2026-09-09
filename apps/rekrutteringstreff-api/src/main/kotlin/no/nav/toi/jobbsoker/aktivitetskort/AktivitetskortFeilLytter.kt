fb7e43b8e88efdee1b8fcdf0ac6ac0716aa25569
Frode.Ankill.Kampe <11638682+frodank@users.noreply.github.com>
2026-09-09 15:37:41 +0200

    AktivitetskortFeilLytter skal også reagere på workop-feil

--- apps/rekrutteringstreff-api/src/main/kotlin/no/nav/toi/jobbsoker/aktivitetskort/AktivitetskortFeilLytter.kt
+++ apps/rekrutteringstreff-api/src/main/kotlin/no/nav/toi/jobbsoker/aktivitetskort/AktivitetskortFeilLytter.kt
@@ -19,7 +19,7 @@ class AktivitetskortFeilLytter(
     init {
         River(rapidsConnection).apply {
             precondition {
-                it.requireAny("@event_name", listOf("aktivitetskort-feil", "aktivitetskort-feil-rekrutteringstreff"))
+                it.requireAny("@event_name", listOf("aktivitetskort-feil", "aktivitetskort-feil-rekrutteringstreff", "aktivitetskort-feil-workop"))
                 it.requireKey("rekrutteringstreffId", "fnr", "endretAv")
                 it.forbid("aktørId")
             }
